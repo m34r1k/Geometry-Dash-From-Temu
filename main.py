5e04af8960c5921acc4aa14d89b53bcd6c6cdca7
m34r1k <m34r1k@naver.com>
2025-12-21 22:34:40 +0900

    주석 달기

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 pygame.init()
 
+#게임 변수 정의
 screen_width = 800
 screen_height = 400
 
@@ -10,15 +11,18 @@ pygame.display.set_caption("Geometry Dash")
 
 clock = pygame.time.Clock()
 
+#색 정의
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 
+#플레이어 관련 변수
 player_x = 100
 player_y = 300
 player_size = 50
 player_color = BLUE
 
+#중력을 만들기 위해 필요한 변수들
 player_v_y = 0 #y축 속도
 gravity = 1.0
 jump_pwr = -17
@@ -30,6 +34,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             
+        #마우스 클릭시 점프
         if event.type == pygame.MOUSEBUTTONDOWN:
             if not is_jumping:
                 player_v_y = jump_pwr
@@ -41,6 +46,7 @@ while running:
     
     player_y += player_v_y
     
+    #만약 중력에 의해 라인보다 밑에 가려고 한다면 고정시키기
     if player_y > 300:
         player_y = 300
         player_v_y = 0
